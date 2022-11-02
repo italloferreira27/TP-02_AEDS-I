101b2b7e510e5f345daef4cb569e7bee5c5b4ce3
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-11-02 20:28:06 -0300

    update

--- Project/headers/funcoes.h
+++ Project/headers/funcoes.h
@@ -106,7 +106,7 @@ void Iniciar(int tamanho_Mat, int Matriz[tamanho_Mat][tamanho_Mat]){
 
     //..........Manipulação de arquivo..........
     FILE *file;
-    file = fopen("../Arquivos-teste/ent.txt", "r");                
+    file = fopen("../Arquivos-teste/entrada_I", "r");                
 
     printf("Entradas:\n");
     while(fgets(linha, 50, file) != NULL){
@@ -166,7 +166,7 @@ int Tamanho_Matriz(){
     char linha_conta[50];
 
     FILE *file2;
-    file2 = fopen("../Arquivos-teste/ent.txt", "r");
+    file2 = fopen("../Arquivos-teste/entrada_I", "r");
 
     while(fgets(linha_conta, 50, file2) != NULL){
         tamanho_Mat++;
